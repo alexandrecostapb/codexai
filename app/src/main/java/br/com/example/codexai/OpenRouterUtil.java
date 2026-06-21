5ad7ba54812319dd6fa30088c6e62cb4c6578285
Iasmim Anahí <iasmim.anahi@academico.ifpb.edu.br>
2026-06-21 14:23:02 -0300

    Adicioção de fotos e links no sobre

--- app/src/main/java/br/com/example/codexai/OpenRouterUtil.java
+++ app/src/main/java/br/com/example/codexai/OpenRouterUtil.java
@@ -2,12 +2,8 @@ package br.com.example.codexai;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-
 import java.io.IOException;
 import java.util.ArrayList;
-
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -18,9 +14,9 @@ import okhttp3.Response;
 
 
 public class OpenRouterUtil {
-    private static final String API_KEY = ""; // Aqui você insere sua própria API
+    private static final String POE_API_KEY = ""; // Aqui você insere sua própria API
     private static final String BASE_URL = "https://openrouter.ai/api/v1/chat/completions"; // URL padrão da API
-    private static final String MODEL = "openrouter/free"; // Aqui você insere o nome da IA que vai ser utilizada
+    private static final String MODEL = "openai/gpt-4o-mini"; // Aqui você insere o nome da IA que vai ser utilizada
 
     private final OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(60, java.util.concurrent.TimeUnit.SECONDS)
@@ -39,7 +35,11 @@ public class OpenRouterUtil {
             JSONArray messagesArray = new JSONArray();
             JSONObject system = new JSONObject();
             system.put("role", "system");
-            system.put("content",Prompt.systemPrompt);
+            system.put("content",
+                    "Você é o CodexAI, uma IA que ajuda estudantes a aprender programação. " +
+                            "Responda sempre em português, de forma clara, didática e objetiva. \n" +
+                            "Não repita apresentações ou saudações." //colocar a mensagem incial da ia depois
+            );
 
             messagesArray.put(system);
 
@@ -102,7 +102,7 @@ public class OpenRouterUtil {
 
             Request request = new Request.Builder()
                     .url(BASE_URL)
-                    .addHeader("Authorization", "Bearer " + API_KEY)
+                    .addHeader("Authorization", "Bearer " + POE_API_KEY)
                     .addHeader("Content-Type", "application/json")
                     .addHeader("HTTP-Referer", "https://javacode.app")
                     .addHeader("X-Title", "JavaCode")
--- app/src/main/java/br/com/example/codexai/Sobre.java
+++ app/src/main/java/br/com/example/codexai/Sobre.java
@@ -2,6 +2,7 @@ package br.com.example.codexai;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -13,8 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Sobre extends AppCompatActivity {
-
     private ImageButton btnHomeVoltar;
+    private void abrirNavegador(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,21 +29,72 @@ public class Sobre extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         btnHomeVoltar = findViewById(R.id.btnVoltar);
-
-
-        getWindow().setStatusBarColor(
-                Color.parseColor("#0A111A")
-        );
-        getWindow().setNavigationBarColor(
-                Color.parseColor("#344468")
-        );
+        getWindow().setStatusBarColor(Color.parseColor("#0A111A"));
+        getWindow().setNavigationBarColor(Color.parseColor("#344468"));
 
         btnHomeVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        //Arlindo
+        findViewById(R.id.linkGitArlindo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirNavegador("https://github.com/arlindofranklin1");
+            }
+        });
+        findViewById(R.id.linkLinkedinArlindo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirNavegador("https://www.linkedin.com/in/arlindo-franklim-62b6a3409?utm_source=share_via&utm_content=profile&utm_medium=member_android");
+            }
+        });
+
+        //Iasmim
+        findViewById(R.id.linkGitIasmim).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirNavegador("https://github.com/iasmimanahi");
+            }
+        });
+        findViewById(R.id.linkLinkedinIasmim).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirNavegador("https://www.linkedin.com/in/iasmim-anah%C3%AD-14a25240a/");
+            }
+        });
+
+        //Kauã
+        findViewById(R.id.linkGitKaua).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirNavegador("https://github.com/kauazinn2109");
+            }
+        });
+        findViewById(R.id.linkLinkedinKaua).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirNavegador("https://www.linkedin.com/in/kau%C3%A3-lucas-a98693409/");
+            }
+        });
+
+        //Alexandre
+        findViewById(R.id.linkGitAlexandre).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirNavegador("https://github.com/alexandrecostapb");
+            }
+        });
+        findViewById(R.id.linkLinkedinAlexandre).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirNavegador("https://www.linkedin.com/in/alexandrecostapb/");
             }
         });
     }
