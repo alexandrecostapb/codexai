78357fe98a609c7fc1a2a5316c6b031654dc48a4
Iasmim Anahí <iasmim.anahi@academico.ifpb.edu.br>
2026-07-05 15:31:59 -0300

    Ajustes no sobre

--- app/src/main/java/br/com/example/codexai/OpenRouterUtil.java
+++ app/src/main/java/br/com/example/codexai/OpenRouterUtil.java
@@ -35,12 +35,7 @@ public class OpenRouterUtil {
             JSONArray messagesArray = new JSONArray();
             JSONObject system = new JSONObject();
             system.put("role", "system");
-            system.put("content",
-                    "Você é o CodexAI, uma IA que ajuda estudantes a aprender programação. " +
-                            "Responda sempre em português, de forma clara, didática e objetiva. \n" +
-                            "Não repita apresentações ou saudações." //colocar a mensagem incial da ia depois
-            );
-
+            system.put("content", Prompt.systemPrompt);
             messagesArray.put(system);
 
             for (Mensagem m : mensagens) {
